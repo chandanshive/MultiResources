f605b3b992abc4d81ff96e7ff397428185934d23
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2021-09-13 10:38:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,203 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo1" {
+  bucket = "my-tf-test-bucket1"
+
+  tags = {
+    Name = "bucketdemo1"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo2" {
+  bucket = "my-tf-test-bucket2"
+
+  tags = {
+    Name = "bucketdemo2"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo3" {
+  bucket = "my-tf-test-bucket3"
+
+  tags = {
+    Name = "bucketdemo3"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo4" {
+  bucket = "my-tf-test-bucket4"
+
+  tags = {
+    Name = "bucketdemo4"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo5" {
+  bucket = "my-tf-test-bucket5"
+
+  tags = {
+    Name = "bucketdemo5"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo6" {
+  bucket = "my-tf-test-bucket6"
+
+  tags = {
+    Name = "bucketdemo6"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo7" {
+  bucket = "my-tf-test-bucket7"
+
+  tags = {
+    Name = "bucketdemo7"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo8" {
+  bucket = "my-tf-test-bucket8"
+
+  tags = {
+    Name = "bucketdemo8"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo9" {
+  bucket = "my-tf-test-bucket9"
+
+  tags = {
+    Name = "bucketdemo9"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo10" {
+  bucket = "my-tf-test-bucket10"
+
+  tags = {
+    Name = "bucketdemo10"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo11" {
+  bucket = "my-tf-test-bucket11"
+
+  tags = {
+    Name = "bucketdemo11"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo12" {
+  bucket = "my-tf-test-bucket12"
+
+  tags = {
+    Name = "bucketdemo12"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo13" {
+  bucket = "my-tf-test-bucket13"
+
+  tags = {
+    Name = "bucketdemo13"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo14" {
+  bucket = "my-tf-test-bucket14"
+
+  tags = {
+    Name = "bucketdemo14"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo15" {
+  bucket = "my-tf-test-bucket15"
+
+  tags = {
+    Name = "bucketdemo15"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo16" {
+  bucket = "my-tf-test-bucket16"
+
+  tags = {
+    Name = "bucketdemo16"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo17" {
+  bucket = "my-tf-test-bucket17"
+
+  tags = {
+    Name = "bucketdemo17"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo18" {
+  bucket = "my-tf-test-bucket18"
+
+  tags = {
+    Name = "bucketdemo18"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo19" {
+  bucket = "my-tf-test-bucket19"
+
+  tags = {
+    Name = "bucketdemo19"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo20" {
+  bucket = "my-tf-test-bucket20"
+
+  tags = {
+    Name = "bucketdemo20"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo21" {
+  bucket = "my-tf-test-bucket21"
+
+  tags = {
+    Name = "bucketdemo21"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo22" {
+  bucket = "my-tf-test-bucket22"
+
+  tags = {
+    Name = "bucketdemo22"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo23" {
+  bucket = "my-tf-test-bucket23"
+
+  tags = {
+    Name = "bucketdemo23"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo24" {
+  bucket = "my-tf-test-bucket24"
+
+  tags = {
+    Name = "bucketdemo24"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo25" {
+  bucket = "my-tf-test-bucket25"
+
+  tags = {
+    Name = "bucketdemo25"
+  }
+}
